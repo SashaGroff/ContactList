3ef6ecc32120a2c2dbb18e55f8f1dc92af612700
SashaGroff <goncharov.der@gmail.com>
2023-12-14 15:45:49 +0300

    Work with logic and UI

---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Aleksandr Goncharov on 14.12.2023.
+//
+
+import Foundation
---
+++ ContactList/Services/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  ContactList
+//
+//  Created by Aleksandr Goncharov on 14.12.2023.
+//
+
+import Foundation
