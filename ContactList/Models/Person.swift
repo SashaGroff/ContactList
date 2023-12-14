35e1574f1d54a7469da330529034f395203dc660
SashaGroff <goncharov.der@gmail.com>
2023-12-14 16:10:54 +0300

    Add DataStore

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -5,4 +5,4 @@
 //  Created by Aleksandr Goncharov on 14.12.2023.
 //
 
-import Foundation
+
--- ContactList/Services/DataStore.swift
+++ ContactList/Services/DataStore.swift
@@ -5,4 +5,59 @@
 //  Created by Aleksandr Goncharov on 14.12.2023.
 //
 
-import Foundation
+final class DataStore {
+    
+    static let share = DataStore()
+    
+    let names = [
+        "Aleksandr",
+        "Rustam",
+        "Anton",
+        "Maksim",
+        "Diana",
+        "Egor",
+        "Ivan",
+        "Natalia",
+        "Pavel",
+        "Ekaterina"
+    ]
+    
+    let surnames = [
+        "Lee",
+        "May",
+        "Clarkson",
+        "Lindemann",
+        "Reus",
+        "Cooper",
+        "Holmes",
+        "Vaananen",
+        "Conte",
+        "Gosling"
+    ]
+    
+    let phoneNumbers = [
+        "+1 555-123-4567",
+        "+49 123-456-7890",
+        "+33 555-888-9999",
+        "+7 987-654-3210",
+        "+44 123-456-7890",
+        "+86 555-888-9999",
+        "+91 987-654-3210",
+        "+55 123-456-7890",
+        "+61 555-888-9999",
+        "+27 987-654-3210"
+    ]
+    
+    let emails = [
+        "gitaw-ocosi98@hotmail.com ",
+        "garu_siwori92@outlook.com",
+        "vifi_kiluga77@mail.com",
+        "yizejix_ica1@aol.com",
+        "deg_ezevule49@outlook.com",
+        "peyet_uvomu22@hotmail.com",
+        "cato-digadu60@gmail.com",
+        "hah_ojobobi34@outlook.com",
+        "becer_ikaja62@hotmail.com",
+        "fupud_itoye60@gmail.com"
+    ]
+}
